0a72207cd4997352268bf92b289e25841b010bc1
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-07-03 21:03:52 +0200

    Add argparser

--- wrapper.py
+++ wrapper.py
@@ -2,7 +2,46 @@
 
 import yaml
 import os
+import sys
 import datetime
+import argparse
+import subprocess as sp
+
+TEST_MODES = ['seqwr', 'seqrewr', 'seqrd', 'rndrd', 'rndwr', 'rndrw']
+DEFAULT_MODE = 'rndrw'
+
+DISK_TYPES = ['hdd', 'ssd', 'ram']
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-o', '--output',
+                    type=argparse.FileType('a'), default=sys.stdout)
+
+# sysbench params
+parser.add_argument('-s', '--file-total-size',
+                    action='store', dest='total_size', required=True)
+parser.add_argument('-m', '--file-test-mode',
+                    action='store', dest='test_mode', choices=TEST_MODES, default=DEFAULT_MODE)
+parser.add_argument('-t', '--time',
+                    action='store', dest='time', default=300)
+
+# metadata
+parser.add_argument('--disk',
+                    action='store', choices=DISK_TYPES, required=True)
+parser.add_argument('--fs',
+                    action='store', required=True)
+parser.add_argument('-c', '--compression',
+                    action='store', default=None)
+parser.add_argument('--raid',
+                    action='store', default=None)
+parser.add_argument('--host',
+                    action='store', default=None,
+                    help='Computer performing the benchmark')
+
+
+[args, passthru_args] = parser.parse_known_args()
+
+
 
 
 def parse(lines):
