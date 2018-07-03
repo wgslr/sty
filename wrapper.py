84ba9a3618fb27cd0f14c5c51640530282ae3d4f
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-07-03 21:20:01 +0200

    Run sysbench from the wrapper

--- wrapper.py
+++ wrapper.py
@@ -20,10 +20,12 @@ parser.add_argument('-o', '--output',
 # sysbench params
 parser.add_argument('-s', '--file-total-size',
                     action='store', dest='total_size', required=True)
+parser.add_argument('-n', '--file-num',
+                    action='store', type=int, dest='file_num', default=128)
 parser.add_argument('-m', '--file-test-mode',
                     action='store', dest='test_mode', choices=TEST_MODES, default=DEFAULT_MODE)
 parser.add_argument('-t', '--time',
-                    action='store', dest='time', default=300)
+                    action='store', type=int, dest='time', default=300)
 
 # metadata
 parser.add_argument('--disk',
@@ -41,7 +43,16 @@ parser.add_argument('--host',
 
 [args, passthru_args] = parser.parse_known_args()
 
+print(args)
+print(passthru_args)
 
+def make_sysbench_args():
+    result = passthru_args
+    result.append('--file-total-size={}'.format(args.total_size))
+    result.append('--file-test-mode={}'.format(args.test_mode))
+    result.append('--time={}'.format(args.time))
+    result.append('--file-num={}'.format(args.file_num))
+    return result
 
 
 def parse(lines):
@@ -50,21 +61,40 @@ def parse(lines):
     lines = [l.lower() for l in lines]
     return yaml.load('\n'.join(lines))
 
-    
+
+def run(bench_args):
+    print("Running ", ['sysbench', 'fileio', 'prepare'] + bench_args)
+    o1 = sp.check_output(['sysbench', 'fileio', 'prepare'] + bench_args, encoding='utf8')
+    print(o1)
+    o2 = sp.check_output(['sysbench', 'fileio', 'run'] + bench_args, encoding='utf8')
+    print(o2)
+    o3 = sp.check_output(['sysbench', 'fileio', 'cleanup'] + bench_args, encoding='utf8')
+    print(o3)
+
+
+
+
 if __name__ == '__main__':
-    name = 'report.txt'
-    with open(name) as f:
-        lines = f.readlines()
-        results = parse(lines)
+    bench_args = make_sysbench_args()
+    run(bench_args)
 
-        timestamp = datetime.datetime.now().isoformat()
 
-        record = {
-            'timestamp': timestamp,
-            'path': os.getcwd(),
-            'filesystem': '',
-            'compression': 'none'
-        }
-        record['results'] = results
 
-        print(yaml.dump([record], default_flow_style=False))
+    # name = 'report.txt'
+    # with open(name) as f:
+    #     lines = f.readlines()
+    #     results = parse(lines)
+
+    #     timestamp = datetime.datetime.now().isoformat()
+
+    #     # TODO test mode and time
+
+    #     record = {
+    #         'timestamp': timestamp,
+    #         'path': os.getcwd(),
+    #         'filesystem': '',
+    #         'compression': 'none'
+    #     }
+    #     record['results'] = results
+
+    #     print(yaml.dump([record], default_flow_style=False))
