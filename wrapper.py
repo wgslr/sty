a3cd3aa3ecf8811b34e5a723d4a9ad8bc87f9054
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-07-04 21:31:34 +0200

    Split sysbench output by "paragraphs"

--- wrapper.py
+++ wrapper.py
@@ -12,6 +12,8 @@ DEFAULT_MODE = 'rndrw'
 
 DISK_TYPES = ['hdd', 'ssd', 'ram']
 
+RAID_TYPES = ['none', 'simple', '0', '1', '5', '6', '10']
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-o', '--output',
@@ -26,75 +28,99 @@ parser.add_argument('-m', '--file-test-mode',
                     action='store', dest='test_mode', choices=TEST_MODES, default=DEFAULT_MODE)
 parser.add_argument('-t', '--time',
                     action='store', type=int, dest='time', default=300)
+parser.add_argument('--no-validate',
+                    action='store_false',  dest='validate')
 
 # metadata
 parser.add_argument('--disk',
                     action='store', choices=DISK_TYPES, required=True)
 parser.add_argument('--fs',
-                    action='store', required=True)
+                    action='store', dest='filesystem', required=True)
 parser.add_argument('-c', '--compression',
-                    action='store', default=None)
+                    action='store', default='none')
+parser.add_argument('--encryption',
+                    action='store', default='none')
 parser.add_argument('--raid',
-                    action='store', default=None)
+                    action='store', default='none', choices=RAID_TYPES)
 parser.add_argument('--host',
-                    action='store', default=None,
+                    action='store', default=os.uname().nodename,
                     help='Computer performing the benchmark')
 
 
 [args, passthru_args] = parser.parse_known_args()
 
-print(args)
-print(passthru_args)
+
+def run(bench_args):
+    print("Running ", ' '.join(['sysbench', 'fileio', 'prepare'] + bench_args))
+    sp.check_call(['sysbench', 'fileio', 'prepare'] +
+                  bench_args, encoding='utf8')
+
+    print("Running ", ' '.join(['sysbench', 'fileio', 'run'] + bench_args))
+    output = sp.check_output(
+        ['sysbench', 'fileio', 'run'] + bench_args, encoding='utf8')
+
+    print("Running ", ' '.join(['sysbench', 'fileio', 'cleanup'] + bench_args))
+    sp.check_call(['sysbench', 'fileio', 'cleanup'] +
+                  bench_args, encoding='utf8')
+
+    return output
+
 
 def make_sysbench_args():
-    result = passthru_args
+    result = [] + passthru_args
     result.append('--file-total-size={}'.format(args.total_size))
     result.append('--file-test-mode={}'.format(args.test_mode))
     result.append('--time={}'.format(args.time))
     result.append('--file-num={}'.format(args.file_num))
+    if args.validate:
+        result.append('--validate')
     return result
 
 
-def parse(lines):
-    # strip non-yaml compatbiel
-    lines = lines[22:]
-    lines = [l.lower() for l in lines]
-    return yaml.load('\n'.join(lines))
+def parse(output):
+    parts = output.lower().split('\n\n')
+
+    yamlish = parts[-5:]
+
+    return yaml.load('\n\n'.join(yamlish))
 
 
-def run(bench_args):
-    print("Running ", ['sysbench', 'fileio', 'prepare'] + bench_args)
-    o1 = sp.check_output(['sysbench', 'fileio', 'prepare'] + bench_args, encoding='utf8')
-    print(o1)
-    o2 = sp.check_output(['sysbench', 'fileio', 'run'] + bench_args, encoding='utf8')
-    print(o2)
-    o3 = sp.check_output(['sysbench', 'fileio', 'cleanup'] + bench_args, encoding='utf8')
-    print(o3)
+def gather_metadata():
+    timestamp = datetime.datetime.now().isoformat()
+    path = os.getcwd()
+
+    return {
+        'timestamp': timestamp,
+        'path': path,
+        'host': args.host,
+        'filesystem': args.filesystem,
+        'compression': args.compression,
+        'raid': args.raid,
+        'disk': args.disk,
+        'encryption': args.encryption
+    }
 
 
+def gather_params():
+    return {
+        'file-total-size': args.total_size,
+        'file-test-mode': args.test_mode,
+        'time': args.time,
+        'file-num': args.file_num,
+        'validate': args.validate,
+        'other': passthru_args
+    }
 
 
 if __name__ == '__main__':
     bench_args = make_sysbench_args()
-    run(bench_args)
+    output = run(bench_args)
 
+    record = {
+        'results': parse(output),
+        'metadata': gather_metadata(),
+        'params': gather_params()
+    }
 
-
-    # name = 'report.txt'
-    # with open(name) as f:
-    #     lines = f.readlines()
-    #     results = parse(lines)
-
-    #     timestamp = datetime.datetime.now().isoformat()
-
-    #     # TODO test mode and time
-
-    #     record = {
-    #         'timestamp': timestamp,
-    #         'path': os.getcwd(),
-    #         'filesystem': '',
-    #         'compression': 'none'
-    #     }
-    #     record['results'] = results
-
-    #     print(yaml.dump([record], default_flow_style=False))
+    yml = yaml.dump([record], default_flow_style=False)
+    args.output.write(yml)
